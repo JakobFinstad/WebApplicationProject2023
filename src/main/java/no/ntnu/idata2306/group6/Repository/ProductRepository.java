f469d5a7a803a307c132ea7787a4398dbbcc6ee4
Harald <haraldwf@stud.ntnu.no>
2023-02-17 13:26:29 +0100

    extends crudrepository

--- src/main/java/no/ntnu/idata2306/group6/Repository/ProductRepository.java
+++ src/main/java/no/ntnu/idata2306/group6/Repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package no.ntnu.idata2306.group6.Repository;
 
-public interface ProductRepository {
+import no.ntnu.idata2306.group6.logic.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 }
