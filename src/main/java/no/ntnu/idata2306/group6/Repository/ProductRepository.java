b2fc6d8ae374028a56ef48bf8ad5342c21b7caf9
Harald <haraldwf@stud.ntnu.no>
2023-02-17 13:45:54 +0100

    Finished productRepository

--- src/main/java/no/ntnu/idata2306/group6/Repository/ProductRepository.java
+++ src/main/java/no/ntnu/idata2306/group6/Repository/ProductRepository.java
@@ -3,5 +3,8 @@ package no.ntnu.idata2306.group6.Repository;
 import no.ntnu.idata2306.group6.logic.Product;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Handle SQL database access, for products.
+ */
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 }
