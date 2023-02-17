0292c9474143c0acd390e9e2c73ea5a448583bea
Harald <haraldwf@stud.ntnu.no>
2023-02-17 13:25:55 +0100

    Add and delete method productController

--- src/main/java/no/ntnu/idata2306/group6/Controller/ProductController.java
+++ src/main/java/no/ntnu/idata2306/group6/Controller/ProductController.java
@@ -2,8 +2,12 @@ package no.ntnu.idata2306.group6.Controller;
 
 import no.ntnu.idata2306.group6.Repository.ProductRepository;
 import no.ntnu.idata2306.group6.logic.Product;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.*;
 
@@ -13,19 +17,94 @@ import java.util.*;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-  private Map<Integer, Product> products;
-  private int latestId;
   private ProductRepository productRepository;
 
-  public ProductController() {initializeData();}
+  private static final Logger logger = LoggerFactory.getLogger(ProductController.class.getSimpleName());
 
-  /**
-   * Initialize dummy product data for the collection
-   */
-  private void initializeData() {
-    latestId = 1;
-    products = new HashMap<>();
-    //addProductToCollection(new Product())
+  public ProductController(ProductRepository productRepository) {
+    this.productRepository = productRepository;
   }
 
+  /**
+   * Get all products
+   * HTTP GET to /product
+   *
+   * @return List of all products currently stored in the collection; or 500 code on error.
+   */
+  @GetMapping
+  @Operation(
+      summary = "Get all products",
+      description = "List of all products currently stored in collection"
+  )
+  public ResponseEntity<Object> getAll() {
+    logger.error("Getting all ");
+    Iterable<Product> products = productRepository.findAll();
+    return new ResponseEntity<>(products, HttpStatus.OK);
+  }
+
+  /**
+   * Get a specific product
+   *
+   * @param id ID of the returned product
+   * @return Product with the given ID or status 404
+   */
+   @GetMapping("/{id}")
+  public ResponseEntity<Product> getOne(@PathVariable Integer id) {
+    ResponseEntity<Product> response;
+    Optional<Product> product = productRepository.findById(id);
+    if (product.isPresent()) {
+      response = new ResponseEntity<>(product.get(), HttpStatus.OK);
+    } else {
+      response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return response;
+   }
+
+  /**
+   * HTTP POST endpoint for adding a new product.
+   *
+   * @param product Data of the book to add. ID will be ignored.
+   * @return 201 Created on success and the new ID in the response body,
+   * 400 Bad request if some data is missing or incorrect
+   */
+  @PostMapping()
+  @Operation(deprecated = true)
+  ResponseEntity<String> add(@RequestBody Product product) {
+    ResponseEntity<String> response;
+
+    try {
+      addProductToCollection(product);
+      response = new ResponseEntity<>("" + product.getProductId(), HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+      response = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    return response;
+  }
+
+  private void addProductToCollection(Product product) {
+
+  }
+
+  /**
+   * Delete a product from the collection
+   *
+   * @param id ID of the product to delete
+   * @return 200 OK on success, 404 Not found on error
+   */
+  @DeleteMapping("/{id}")
+  @Operation(hidden = true)
+  public ResponseEntity<String> delete(@PathVariable int id) {
+    ResponseEntity<String> response;
+    if (removeProductFromCollection(id)) {
+      response = new ResponseEntity<>(HttpStatus.OK);
+    } else {
+      response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return response;
+  }
+
+  private boolean removeProductFromCollection(int id) {
+    return true;
+  }
 }
