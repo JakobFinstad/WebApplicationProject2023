f4aac5266a32a16795552b405abfcd8b8dacaf26
JakobFinstad <jakobf44@gmail.com>
2023-03-23 13:40:15 +0100

    Finished authenticationn controller

--- src/main/java/no/ntnu/idata2306/group6/Controller/AuthenticationController.java
+++ src/main/java/no/ntnu/idata2306/group6/Controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package no.ntnu.idata2306.group6.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,5 +23,17 @@ public class AuthenticationController {
     private JwtUtil jwtUtil;
 
 
-    @PostMapping("")
+    @PostMapping("/authentication")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),authenticationRequest.getPassword()
+            ));
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+        }
+        final UserDetails userDetails = userDetailsService.loadByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationRespons(jwt));
+    }
 }
