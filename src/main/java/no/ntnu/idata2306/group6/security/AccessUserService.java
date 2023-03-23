3822c87f19d1ca3a0ca6733c0fc0440d3d0c52f8
JakobFinstad <jakobf44@gmail.com>
2023-03-23 15:41:57 +0100

    Added some importstatements to solve redlines

--- src/main/java/no/ntnu/idata2306/group6/security/AccessUserService.java
+++ src/main/java/no/ntnu/idata2306/group6/security/AccessUserService.java
@@ -3,6 +3,9 @@ package no.ntnu.idata2306.group6.security;
 import no.ntnu.idata2306.group6.logic.User;
 import no.ntnu.idata2306.group6.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
