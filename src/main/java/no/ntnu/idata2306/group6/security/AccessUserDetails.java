3076e7eac564da506f9ce6ff798dfc4c61071e5f
JakobFinstad <jakobf44@gmail.com>
2023-03-23 14:14:45 +0100

    Changes in the pom for the security dependency and finished AccessUserDetails

--- src/main/java/no/ntnu/idata2306/group6/security/AccessUserDetails.java
+++ src/main/java/no/ntnu/idata2306/group6/security/AccessUserDetails.java
@@ -1,4 +1,71 @@
 package no.ntnu.idata2306.group6.security;
 
-public class AccessUserDetails {
+import no.ntnu.idata2306.group6.logic.Role;
+import no.ntnu.idata2306.group6.logic.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Contains authentication information, needed by the UserDetailsService.
+ */
+public class AccessUserDetails implements UserDetails {
+    private final String username;
+    private final String password;
+    private final boolean isActive;
+    private final List<GrantedAuthority> authorities = new LinkedList<>();
+
+    public AccessUserDetails(User user) {
+        this.username = user.getEmail();
+        this.password = user.getPassword();
+        this.isActive = user.isActive();
+        this.convertRoles(user.getRoles());
+    }
+
+    private void convertRoles(Set<Role> roles) {
+        authorities.clear();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+    }
+
+    @Override
+    public Collection<? extends  GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 }
