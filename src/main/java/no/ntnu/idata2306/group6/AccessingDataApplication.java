922def694eb4054bdffab29ff5cf177a750ec460
Harald <haraldwf@stud.ntnu.no>
2023-04-17 14:22:44 +0200

    Javadoc application method

--- src/main/java/no/ntnu/idata2306/group6/AccessingDataApplication.java
+++ src/main/java/no/ntnu/idata2306/group6/AccessingDataApplication.java
@@ -3,6 +3,9 @@ package no.ntnu.idata2306.group6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entrypoint for the application.
+ */
 @SpringBootApplication
 public class AccessingDataApplication {
     public static void main(String[] args) {
