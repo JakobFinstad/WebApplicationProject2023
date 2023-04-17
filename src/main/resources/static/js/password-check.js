87290ae36829a65167e9dd5f9be44258a76fea40
JakobFinstad <jakobf44@gmail.com>
2023-04-17 15:55:40 +0200

    Finished login and signup pages

--- src/main/resources/static/js/password-check.js
+++ src/main/resources/static/js/password-check.js
@@ -1,9 +1,11 @@
 function check() {
     const input = document.getElementById('password-confirm');
     if ((document.getElementById('password-confirm').value != document.getElementById('password').value)) {
-        input.setCustomValidity('Password Must be Matching.');
+        input.setCustomValidity('pASSword NOT MaTChIng!');
     } else {
         // input is valid -- reset the error message
         input.setCustomValidity('');
     }
 }
+
+document.getElementById("submit").addEventListener("submit", check);
