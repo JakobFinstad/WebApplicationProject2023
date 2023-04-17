9aa2ddd16c926a000b7af3edc6bdb4918e301f53
Harald <haraldwf@stud.ntnu.no>
2023-04-17 12:53:25 +0200

    Rename main class

--- src/main/java/no/ntnu/idata2306/group6/AccessingDataApplication.java
+++ src/main/java/no/ntnu/idata2306/group6/AccessingDataApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class AccessingDataApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(AccessingDataApplication.class, args);
     }
 }
