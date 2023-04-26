6e0e5169d95411fb2227605ef44c0ac4c395afb4
JakobFinstad <jakobf44@gmail.com>
2023-04-26 12:31:09 +0200

    Redirected the authentication methods

--- src/main/java/no/ntnu/idata2306/group6/controller/AuthenticationController.java
+++ src/main/java/no/ntnu/idata2306/group6/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@RequestMapping("/api")
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
