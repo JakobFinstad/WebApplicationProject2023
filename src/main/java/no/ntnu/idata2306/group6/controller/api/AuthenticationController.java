605eeb51cfadea18fdf485ebd42802a22ca55edc
JakobFinstad <jakobf44@gmail.com>
2023-05-30 13:28:06 +0200

    Some changes to the login site

--- src/main/java/no/ntnu/idata2306/group6/controller/api/AuthenticationController.java
+++ src/main/java/no/ntnu/idata2306/group6/controller/api/AuthenticationController.java
@@ -1,14 +1,10 @@
 package no.ntnu.idata2306.group6.controller.api;
 
-import io.swagger.v3.core.util.Json;
 import no.ntnu.idata2306.group6.security.AuthenticationRequest;
 import no.ntnu.idata2306.group6.security.AuthenticationResponse;
 import no.ntnu.idata2306.group6.security.JwtUtil;
 import no.ntnu.idata2306.group6.security.SecurityConfiguration;
-import org.apache.catalina.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,16 +18,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for authentication of the tokens.
- *
  */
-@RestController
+@Controller
 @RequestMapping("/api")
-@Lazy
-public class  AuthenticationController {
+public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
@@ -84,7 +77,7 @@ public class  AuthenticationController {
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String jwt = jwtUtil.generateToken(userDetails);
 
-        return ResponseEntity.ok(new AuthenticationResponse().setJwt(jwt));
+        return new ResponseEntity<>(new AuthenticationResponse().setJwt(jwt), HttpStatus.OK);
     }
 
 
