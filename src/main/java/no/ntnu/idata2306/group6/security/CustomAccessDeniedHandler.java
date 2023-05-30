01b4507305fb2b222507c154b42b2cb0c23592cf
JakobFinstad <jakobf44@gmail.com>
2023-05-30 16:16:49 +0200

    Working on the sequrity config

---
+++ src/main/java/no/ntnu/idata2306/group6/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package no.ntnu.idata2306.group6.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final String ACCESS_DENIED_URL = "/no-access";
+
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect(request.getContextPath() + ACCESS_DENIED_URL);
+    }
+}
--- src/main/java/no/ntnu/idata2306/group6/security/SecurityConfiguration.java
+++ src/main/java/no/ntnu/idata2306/group6/security/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package no.ntnu.idata2306.group6.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,6 +16,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
 
 /**
  * Creates AuthenticationManager - set up authentication type
@@ -58,8 +65,10 @@ public class SecurityConfiguration {
                     .authorizeRequests()
                     .requestMatchers("/css/**", "/images/**", "/js/**", "/templates/**", "/swagger-ui/**").permitAll()
                     .requestMatchers("/", "/home", "/index", "/login", "/signup", "/api/user/signup", "/api/testimonial").permitAll()
+                    .requestMatchers("/login", "/signup").not().hasAnyRole("USER","ADMIN")
                     .requestMatchers("/admin/**").hasRole("ADMIN")
-                    .requestMatchers("/products/**").hasAnyRole("ADMIN", "USER")
+                    .requestMatchers("/profile-page").hasAnyRole("USER", "ADMIN")
+//                    .requestMatchers("/products/**").hasAnyRole("ADMIN", "USER")
                     .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/webjars/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
@@ -67,7 +76,10 @@ public class SecurityConfiguration {
                     .loginPage("/login")
                     .and()
                     .logout()
-                    .logoutSuccessUrl("/");
+                    .logoutSuccessUrl("/").and()
+                    .exceptionHandling()
+                    .accessDeniedHandler(accessDeniedHandler())
+                    .and();
 
 
         } else {
@@ -80,6 +92,10 @@ public class SecurityConfiguration {
 
 
         return http.build();
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
     }
 
 //    @Autowired
