fed6ac7039f863e813eb5b4a1f0e093ad5349ba0
JakobFinstad <jakobf44@gmail.com>
2023-05-31 01:40:41 +0200

    Added swagger docs to index controller

--- src/main/java/no/ntnu/idata2306/group6/controller/web/IndexController.java
+++ src/main/java/no/ntnu/idata2306/group6/controller/web/IndexController.java
@@ -1,12 +1,11 @@
 package no.ntnu.idata2306.group6.controller.web;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
+import io.swagger.v3.oas.annotations.Operation;
 import no.ntnu.idata2306.group6.entity.Info;
 import no.ntnu.idata2306.group6.entity.Product;
 import no.ntnu.idata2306.group6.service.AccessUserService;
 import no.ntnu.idata2306.group6.service.InfoService;
 import no.ntnu.idata2306.group6.service.ProductService;
-import no.ntnu.idata2306.group6.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,16 +16,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+
 @Controller
 @CrossOrigin
 public class IndexController {
 
-
-    private ProductService productService;
-
-    private InfoService infoService;
-
-    private AccessUserService userService;
+    private final ProductService productService;
+    private final InfoService infoService;
+    private final AccessUserService userService;
 
     @Autowired
     public IndexController(ProductService productService, InfoService infoService, AccessUserService userService) {
@@ -35,43 +32,37 @@ public class IndexController {
         this.userService = userService;
     }
 
-    /**
-     * Get the index page of the main site.
-     *
-     * @return index
-     */
     @GetMapping("/")
-    public String home(Model model){
-
-        List<Product> products = this.productService.getRandomProducts();
+    @Operation(
+            summary = "Get the index page of the main site",
+            description = "Returns the HTML page for the main site's index"
+    )
+    public String home(Model model) {
+        List<Product> products = productService.getRandomProducts();
         List<Info> infos = new ArrayList<>();
         for (Product p : products) {
             infos.addAll((Collection<? extends Info>) infoService.findByProdId(p.getProductId()));
-//                     Iterables.concat(infos, infoService.findByProdId(p.getProductId()));
         }
-
         model.addAttribute("featuredProduct", products);
         model.addAttribute("infos", infos);
         model.addAttribute("user", userService.getSessionUser());
         return "index";
     }
 
-    /**
-     * Get the index page of the main site.
-     *
-     * @return index
-     */
     @GetMapping("/home")
+    @Operation(
+            summary = "Get the index page of the main site",
+            description = "Returns the HTML page for the main site's index"
+    )
     public String home2(Model model) {
         return home(model);
     }
 
-    /**
-     * Get the index page of the main site.
-     *
-     * @return index
-     */
     @GetMapping("/index")
+    @Operation(
+            summary = "Get the index page of the main site",
+            description = "Returns the HTML page for the main site's index"
+    )
     public String index(Model model) {
         return home(model);
     }
